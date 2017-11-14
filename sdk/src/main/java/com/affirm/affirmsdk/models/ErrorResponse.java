2b230315c0dab25a439c8b9cbfc089b6bb07fcf8
hector <hector.monserrate@affirm.com>
2017-11-14 14:54:19 -0800

    fix ErrorResponse data class

--- sdk/src/main/java/com/affirm/affirmsdk/models/ErrorResponse.java
+++ sdk/src/main/java/com/affirm/affirmsdk/models/ErrorResponse.java
@@ -1,19 +1,14 @@
 package com.affirm.affirmsdk.models;
 
-import android.support.annotation.Nullable;
 import com.google.auto.value.AutoValue;
 import com.google.gson.Gson;
 import com.google.gson.TypeAdapter;
 import com.google.gson.annotations.SerializedName;
 
-// {"field": "merchant.public_api_key", "message": "You have provided an invalid public API key.",
-// "code": "public-api-key-invalid", "type": "invalid_request", "status_code": 400}
 @AutoValue public abstract class ErrorResponse {
-  @Nullable public abstract String field();
-
   public abstract String message();
 
-  @SerializedName("status_code") public abstract String status();
+  @SerializedName("status_code") public abstract Integer status();
 
   public static TypeAdapter<ErrorResponse> typeAdapter(Gson gson) {
     return new AutoValue_ErrorResponse.GsonTypeAdapter(gson);
