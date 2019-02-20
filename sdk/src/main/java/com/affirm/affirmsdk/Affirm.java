d6c95160db6f6f6b29edf12788382045d8490592
evan <liusuzheng2012@gmail.com>
2019-02-20 11:04:54 +0800

    Pass params to Prequal screen & remove redundant `runOnUiThread `

--- sdk/src/main/java/com/affirm/affirmsdk/Affirm.java
+++ sdk/src/main/java/com/affirm/affirmsdk/Affirm.java
@@ -22,7 +22,6 @@ import static com.affirm.affirmsdk.ModalActivity.ModalType.SITE;
 public final class Affirm {
   private static final int CHECKOUT_REQUEST = 8076;
   private static final int VCN_CHECKOUT_REQUEST = 8077;
-  private static final String REFERRING_URL = "https://androidsdk/";
   private static volatile Affirm instance;
   private String merchant;
   private Environment environment;
@@ -83,6 +82,13 @@ public final class Affirm {
    */
   public static @Nullable Affirm getInstance() {
     return instance;
+  }
+
+  /**
+   * Launches Product Prequal Activity
+   */
+  public void launchPrequal(@NonNull Context context, float amount, @Nullable String promoId) {
+    PrequalActivity.launch(context, merchant, amount, promoId, environment.baseUrl1);
   }
 
   /**
@@ -197,17 +203,9 @@ public final class Affirm {
       @Override public void onClick(View v) {
         boolean showPrequal = (boolean) v.getTag();
         if (showPrequal) {
-          if (TextUtils.isEmpty(promoId)) {
-            PrequalActivity.launch(textView.getContext(), environment.baseUrl1, String.format(
-                    "/apps/prequal?public_api_key=%s&unit_price=%f&isSDK=true&use_promo=True&referring_url=%s",
-                    merchant, amount, REFERRING_URL), REFERRING_URL);
-          } else {
-            PrequalActivity.launch(textView.getContext(), environment.baseUrl1, String.format(
-                    "/apps/prequal?public_api_key=%s&unit_price=%f&promo_external_id=%s&isSDK=true&use_promo=True&referring_url=%s",
-                    merchant, amount, promoId, REFERRING_URL), REFERRING_URL);
-          }
+          launchPrequal(textView.getContext(), amount, promoId);
         } else {
-            launchProductModal(textView.getContext(), amount, null);
+          launchProductModal(textView.getContext(), amount, null);
         }
       }
     });
--- sdk/src/main/java/com/affirm/affirmsdk/PrequalActivity.java
+++ sdk/src/main/java/com/affirm/affirmsdk/PrequalActivity.java
@@ -4,29 +4,42 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.webkit.WebView;
 
+import java.util.HashMap;
+
 public class PrequalActivity extends AppCompatActivity
     implements AffirmWebViewClient.Callbacks, PopUpWebChromeClient.Callbacks {
-  private static final String EXTRA_PATH = "EXTRA_PATH";
   private static final String BASE_URL_EXTRA = "BASE_URL_EXTRA";
-  private static final String REFERRING_URL = "REFERRING_URL";
   private static final String PROTOCOL = "https://";
+  private static final String REFERRING_URL = "https://androidsdk/";
+
+  private static final String AMOUNT = "AMOUNT";
+  private static final String API_KEY = "API_KEY";
+  private static final String PROMO_ID = "PROMO_ID";
+  private static final String MAP_EXTRA = "MAP_EXTRA";
 
   private WebView webView;
   private View progressIndicator;
 
-  private String path;
-  private String referringUrl;
   private String baseUrlExtra;
+  private HashMap<String, String> map;
 
-  static void launch(@NonNull Context context, @NonNull String baseUrl, @NonNull String path, @NonNull String referringUrl) {
+  static void launch(@NonNull Context context, @NonNull String apiKey, float amount, @Nullable String promoId,
+                     @NonNull String baseUrl) {
     final Intent intent = new Intent(context, PrequalActivity.class);
-    intent.putExtra(EXTRA_PATH, path);
-    intent.putExtra(REFERRING_URL, referringUrl);
+
+    final HashMap<String, String> map = new HashMap<>();
+    map.put(AMOUNT, String.valueOf(amount));
+    map.put(API_KEY, apiKey);
+    map.put(PROMO_ID, promoId);
+
     intent.putExtra(BASE_URL_EXTRA, baseUrl);
+    intent.putExtra(MAP_EXTRA, map);
     context.startActivity(intent);
   }
 
@@ -35,12 +48,10 @@ public class PrequalActivity extends AppCompatActivity
     ViewUtils.hideActionBar(this);
 
     if (savedInstanceState != null) {
-      path = savedInstanceState.getString(EXTRA_PATH);
-      referringUrl = savedInstanceState.getString(REFERRING_URL);
+      map = (HashMap<String, String>) savedInstanceState.getSerializable(MAP_EXTRA);
       baseUrlExtra = savedInstanceState.getString(BASE_URL_EXTRA);
     } else {
-      path = getIntent().getStringExtra(EXTRA_PATH);
-      referringUrl = getIntent().getStringExtra(REFERRING_URL);
+      map = (HashMap<String, String>) getIntent().getSerializableExtra(MAP_EXTRA);
       baseUrlExtra = getIntent().getStringExtra(BASE_URL_EXTRA);
     }
 
@@ -56,9 +67,8 @@ public class PrequalActivity extends AppCompatActivity
   @Override protected void onSaveInstanceState(Bundle outState) {
     super.onSaveInstanceState(outState);
 
-    outState.putString(EXTRA_PATH, path);
-    outState.putString(REFERRING_URL, referringUrl);
     outState.putString(BASE_URL_EXTRA, baseUrlExtra);
+    outState.putSerializable(MAP_EXTRA, map);
   }
 
   private void setupWebview() {
@@ -66,7 +76,7 @@ public class PrequalActivity extends AppCompatActivity
     webView.setWebViewClient(new AffirmWebViewClient(this) {
       @Override
       boolean hasCallbackUrl(WebView view, String url) {
-        if (url.equals(referringUrl)) {
+        if (url.equals(REFERRING_URL)) {
           finish();
           return true;
         }
@@ -77,11 +87,17 @@ public class PrequalActivity extends AppCompatActivity
   }
 
   private void loadWebview() {
-    runOnUiThread(new Runnable() {
-      @Override public void run() {
-        webView.loadUrl(PROTOCOL + baseUrlExtra + path);
-      }
-    });
+    String path;
+    if (TextUtils.isEmpty(map.get(PROMO_ID))) {
+      path = String.format(
+              "/apps/prequal?public_api_key=%s&unit_price=%s&isSDK=true&use_promo=True&referring_url=%s",
+              map.get(API_KEY), map.get(AMOUNT), REFERRING_URL);
+    } else {
+      path = String.format(
+              "/apps/prequal?public_api_key=%s&unit_price=%s&promo_external_id=%s&isSDK=true&use_promo=True&referring_url=%s",
+              map.get(API_KEY), map.get(AMOUNT), map.get(PROMO_ID), REFERRING_URL);
+    }
+    webView.loadUrl(PROTOCOL + baseUrlExtra + path);
   }
 
   @Override public void onWebViewCancellation() {
