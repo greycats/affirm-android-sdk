c0704fe1b3fdfd54c7f3f9c9a0610df4d28369ca
Nanda <nanda.dubey@affirm.com>
2019-02-28 09:33:37 -0800

    Unused import removed

--- sdk/src/main/java/com/affirm/affirmsdk/Affirm.java
+++ sdk/src/main/java/com/affirm/affirmsdk/Affirm.java
@@ -7,7 +7,6 @@ import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.SpannableString;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
 import com.affirm.affirmsdk.di.AffirmInjector;
